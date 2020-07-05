b2f488f896a8b1bb9e21ad74df0a721e56c4fff5
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-05 21:28:39 +0200

    Switched to class based configuration and removed XML based configuration

---
+++ core/src/main/java/com/sanjay/AppConfig.java
@@ -0,0 +1,9 @@
+package com.sanjay;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.sanjay")
+public class AppConfig {
+}
--- core/src/main/java/com/sanjay/Main.java
+++ core/src/main/java/com/sanjay/Main.java
@@ -3,16 +3,15 @@ package com.sanjay;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static final Logger log = LoggerFactory.getLogger(Main.class);
-    public static final String CONTEXT_PATH = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Number generated");
-        // == get spring context from beans.xml ==
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONTEXT_PATH);
+        // == get spring context ==
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         // == get number generator bean
         NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
         // == get next number ==
