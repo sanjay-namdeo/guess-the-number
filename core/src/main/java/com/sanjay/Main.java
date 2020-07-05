1ec2e787e148192337acffe82ffb0be8054c3da5
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-05 13:22:16 +0200

    Implement IOC using XML based context

--- core/src/main/java/com/sanjay/Main.java
+++ core/src/main/java/com/sanjay/Main.java
@@ -2,11 +2,24 @@ package com.sanjay;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static final String CONTEXT_PATH = "beans.xml";
 
     public static void main(String[] args) {
-        log.debug("Temperature is set to {} as of {}", "23", "11:40");
+        log.info("Number generated");
+
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONTEXT_PATH);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        int number = numberGenerator.next();
+
+        log.info("Next number is {}", number);
+
+        context.close();
     }
 }
