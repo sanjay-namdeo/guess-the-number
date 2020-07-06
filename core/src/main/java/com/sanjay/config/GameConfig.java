a0b03895bd78f1170f94eba4454ae2f4bef55c9b
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-06 14:33:12 +0200

    Added game.properties file to read game config values from configuration

--- core/src/main/java/com/sanjay/config/GameConfig.java
+++ core/src/main/java/com/sanjay/config/GameConfig.java
@@ -1,12 +1,17 @@
 package com.sanjay.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
-    private int guessCount = 10;
-    private int maxNumber = 100;
+    @Value("${game.guessCount}")
+    private int guessCount;
+    @Value("${game.maxNumber}")
+    private int maxNumber;
 
     @Bean
     public int guessCount() {
