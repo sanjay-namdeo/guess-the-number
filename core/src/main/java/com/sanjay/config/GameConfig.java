c34a88f81a5fe0477a40dcc6eec75f8546d44a7b
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-06 14:34:47 +0200

    Added default values to game properties

--- core/src/main/java/com/sanjay/config/GameConfig.java
+++ core/src/main/java/com/sanjay/config/GameConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource("classpath:config/game.properties")
 public class GameConfig {
-    @Value("${game.guessCount}")
+    @Value("${game.guessCount:20}")
     private int guessCount;
-    @Value("${game.maxNumber}")
+    @Value("${game.maxNumber:5}")
     private int maxNumber;
 
     @Bean
