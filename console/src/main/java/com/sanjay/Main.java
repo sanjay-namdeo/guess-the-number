c266f9c8a46b313ca706d5150393e95034ce04bd
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-06 15:18:04 +0200

    Code cleanup

--- console/src/main/java/com/sanjay/Main.java
+++ console/src/main/java/com/sanjay/Main.java
@@ -10,23 +10,10 @@ public class Main {
     public static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        log.info("Number generated");
         // == get spring context ==
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        // == get number generator bean
-        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
-        // == get next number ==
-        int number = numberGenerator.next();
-        log.info("Next number is {}", number);
 
-        // == get game bean
-        Game game = context.getBean(Game.class);
-
-        // == get message bean
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-        log.info("Called main message and it returned : {}", messageGenerator.getMainMessage());
-        log.info("Called result message and it returned : {}", messageGenerator.getResultMessage());
-
+        // == close context ==
         context.close();
     }
 }
