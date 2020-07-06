2d038c3b81fea33c381385f7719a4b0116eb4152
Sanjay Namdeo <sanjay.namdeo@live.com>
2020-07-06 16:06:39 +0200

    Added project lombok

--- console/src/main/java/com/sanjay/Main.java
+++ console/src/main/java/com/sanjay/Main.java
@@ -1,11 +1,14 @@
 package com.sanjay;
 
 import com.sanjay.config.GameConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
+        log.info("Hello Project Lombok");
         // == get spring context ==
         ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
 
